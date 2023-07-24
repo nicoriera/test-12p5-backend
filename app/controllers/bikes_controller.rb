599d70ec3a53b6becf2fd7dc9e2316be9c608531
nicolas riera <nicolariera@gmail.com>
2023-07-24 13:38:22 +0200

    feat logic controllers

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,52 @@
 class BikesController < ApplicationController
+    belongs_action :set_bike, only: [:show, :edit, :update, :destroy]
+    belongs_action :check_admin, only: [:index, :destroy]
+
+    def index
+        @bikes = Bike.all
+    end
+
+    def show
+    end
+
+    def new
+        @bike = Bike.new
+    end
+
+    def create
+        @bike : Bike.new(bike_params)
+
+        if @bike.save
+            redirect_to bikes_path, notice: "Bike created successfully"
+        else
+            render :new
+        end
+    end
+
+    def update
+        if @bike.update(bike_params)
+            redirect_to bikes_path, notice: "Bike updated successfully"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @bike.destroy
+        redirect_to bikes_path, notice: "Bike deleted successfully"
+    end
+
+    private
+
+    def set_bike
+        @bike = Bike.find(params[:id])
+    end
+
+    def check_admin
+        redirect_to bikes_path, notice: "You are not authorized to perform this action" unless current_user.admin?
+    end
+
+    def bike_params
+        params.require(:bike).permit(:bike_type, :price)
+    end
 end
