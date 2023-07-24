bfe76e59da60bf4a4e3443ace53dcb3b50de0d1e
nicolas riera <nicolariera@gmail.com>
2023-07-24 23:11:17 +0200

    fix puundit and form for subscription

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,35 +1,40 @@
 class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_subscription, only: [:destroy]
 
   def index
-    @subscriptions = Subscription.all
-
+    @subscriptions = policy_scope(Subscription)
   end
 
   def new
     @subscription = Subscription.new
+    @bikes = Bike.all
+    authorize @subscription
   end
 
   def create
-   
     @subscription = current_user.subscriptions.build(subscription_params)
+    @bikes = Bike.all
+    authorize @subscription
     if @subscription.save
         redirect_to subscriptions_path, notice: "You have successfully subscribed"
     else
-        render :new
+      flash[:alert] = "There was an error creating your subscription"
+      redirect_to new_subscription_path
     end 
   end
 
-  def destroy
-    @subscription = Subscription.find(params[:id])
-    
-    if @subscription.cancel
-        redirect_to subscriptions_path, notice: "You have successfully cancelled your subscription"
-    else
-        redirect_to subscriptions_path, alert: "There was an error cancelling your subscription"
-    end
-        render :edit
+def destroy
+  @subscription = Subscription.find(params[:id])
+  authorize @subscription
+
+  if @subscription.cancel
+      redirect_to subscriptions_path, notice: "You have successfully cancelled your subscription"
+  else
+      redirect_to subscriptions_path, alert: "There was an error cancelling your subscription"
   end
+end
+
 
   private
 
@@ -37,5 +42,18 @@ class SubscriptionsController < ApplicationController
     params.require(:subscription).permit(:start_date, :end_date, :bike_id, :period)
   end
 
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path, notice: "You must be logged in to subscribe"
+    end
+  end
 
+  def set_subscription
+    @subscription = Subscription.find(params[:id])
+    unless current_user == @subscription.user || current_user.admin?
+      redirect_to subscriptions_path, alert: "You are not authorized to cancel this subscription"
+    end
+  end
 end
--- spec/subscription_spec.rb
+++ spec/subscription_spec.rb
@@ -7,3 +7,52 @@ RSpec.describe Subscription, type: :model do
         expect(subscription).to be_valid
     end
 end
+
+RSpec.describe Subscription, type: :model do
+  describe "#period_matches_dates" do
+    context "when start date, end date, and period are all present and valid" do
+      let(:subscription) { Subscription.new(start_date: Date.today, end_date: Date.today + 1.month, period: 1) }
+
+      it "does not add an error to the subscription" do
+        subscription.period_matches_dates
+        expect(subscription.errors).to be_empty
+      end
+    end
+
+    context "when end date does not match start date plus period months" do
+      let(:subscription) { Subscription.new(start_date: Date.today, end_date: Date.today + 2.months, period: 1) }
+
+      it "adds an error to the subscription" do
+        subscription.period_matches_dates
+        expect(subscription.errors[:period]).to include("does not match start and end dates")
+      end
+    end
+
+    context "when start date is missing" do
+      let(:subscription) { Subscription.new(end_date: Date.today + 1.month, period: 1) }
+
+      it "does not add an error to the subscription" do
+        subscription.period_matches_dates
+        expect(subscription.errors).to be_empty
+      end
+    end
+
+    context "when end date is missing" do
+      let(:subscription) { Subscription.new(start_date: Date.today, period: 1) }
+
+      it "does not add an error to the subscription" do
+        subscription.period_matches_dates
+        expect(subscription.errors).to be_empty
+      end
+    end
+
+    context "when period is missing" do
+      let(:subscription) { Subscription.new(start_date: Date.today, end_date: Date.today + 1.month) }
+
+      it "does not add an error to the subscription" do
+        subscription.period_matches_dates
+        expect(subscription.errors).to be_empty
+      end
+    end
+  end
+end
