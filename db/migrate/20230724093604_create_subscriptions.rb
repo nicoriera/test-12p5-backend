a6ff558b8ffea79231c653ed6a306a850f773724
nicolas riera <nicolariera@gmail.com>
2023-07-24 11:37:26 +0200

    feat model subscription

---
+++ db/migrate/20230724093604_create_subscriptions.rb
@@ -0,0 +1,12 @@
+class CreateSubscriptions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :subscriptions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :bike, null: false, foreign_key: true
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
