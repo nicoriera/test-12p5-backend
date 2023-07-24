8f263e71ac832b8401da2346a1902dffb06f2972
nicolas riera <nicolariera@gmail.com>
2023-07-24 17:42:43 +0200

    feat form for subscription

--- spec/subscription_spec.rb
+++ spec/subscription_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Subscription, type: :model do
 
     it 'is valid with valid attributes' do
-        subscription = Subscription.new(start_date: Date.today, end_date: Date.today + 1.month, user_id: 1, bike_id: 1)
+        subscription = Subscription.new(start_date: Date.today, end_date: Date.today + 1.month, user_id: 1, bike_id: 1, period: "one_month")
         expect(subscription).to be_valid
     end
-    end
+end
