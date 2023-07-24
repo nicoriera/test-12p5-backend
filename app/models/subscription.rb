ce70b63c7a59359893e015f265ee1db3df1f829d
nicolas riera <nicolariera@gmail.com>
2023-07-24 12:03:34 +0200

    feat seed

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,9 @@
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :bike
+
+  def cancel
+    self.destroy
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+puts "Cleaning database..."
+Bike.destroy_all
+User.destroy_all
+Subscription.destroy_all
+
+puts "Creating seeds..."
+
+puts "Creating bikes..."
+bike_classic = Bike.create(bike_type: "bike_classic", price: 10.00)
+bike_long = Bike.create(bike_type: "bike_long", price: 15.00)
+bike_cargo = Bike.create(bike_type: "bike_cargo", price: 20.00)
+
+puts "Creating users..."
+user = User.create(name: "John Doe", email: "john.doe@example.com", password: "password")
+admin = User.create(name: "Admin User", email: "admin@example.com", password: "password", role: "admin")
+
+puts "Creating subscriptions..."
+Subscription.create(user: user, bike: bike_classic, start_date: Date.today, end_date: Date.today + 1.month)
+
+puts "Done!"
