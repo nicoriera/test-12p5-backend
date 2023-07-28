3b0a1f9b01a063fb1778c0e7d70e0f0a9c0dbc58
nicolas riera <nicolariera@gmail.com>
2023-07-28 17:53:36 +0200

    update rspec and model and link deconnexion

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "bikes#index"
-  resources :users
-  resources :bikes
-  resources :subscriptions
+  resources :users, only: [:index, :show]
+  resources :bikes, only: [:index, :show]
+  resources :subscriptions, only: [:index, :show, :new, :create, :destroy]
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it 'is valid with valid attributes' do
-        user = User.new(first_name: 'John', last_name: 'Doe', email: 'jogndoe@example.com', password: 'password', role: :user)
-        expect(user).to be_valid
-    end
+  let(:bike) { Bike.create!(bike_type: 'Mountain', price: 10) }
+
+  it "is valid with valid attributes" do
+    user = User.new(email: 'test@example.com', password: 'password')
+    expect(user).to be_valid
+  end
+
+  it "has user role by default" do
+    user = User.new(email: 'test@example.com', password: 'password')
+    expect(user.role).to eq("user")
+  end
+
+  it "can have an admin role" do
+    user = User.new(email: 'test@example.com', password: 'password', role: :admin)
+    expect(user.role).to eq("admin")
+  end
+
 end
